5a2621f0f2295dd0a073a974b5a779ece3aec9f7
izaelgs <izael-123@outlook.com>
2022-10-11 17:18:12 -0300

    background color correta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,10 +20,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
-<body class="antialiased" id="main">
+<body class="antialiased">
     <nav class="navbar fixed-top navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#main">
+            <a class="navbar-brand" href="#">
                 <h1>UniTech</h1>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
