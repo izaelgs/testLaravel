1d45511005837d10576dff82064acc6469008440
izaelgs <izael-123@outlook.com>
2022-10-20 14:37:20 -0300

    footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,23 +1,20 @@
-        <footer class="" id="contato">
-            <section class="container d-flex flex-sm-row justify-content-around text-light py-4" id="sobre">
-                <div class="">
-                    <h4>Contatos</h4>
-                    <ul>
-                        <li>(27) 99677-5765</li>
-                        <li>(27) 92738-3529</li>
-                        <li>(27) 98575-5122</li>
-                    </ul>
-                </div>
-                <div class="">...
-                </div>
-                <div class="">...
-                </div>
-            </section>
-        </footer>
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <!-- <script src="{{ url(mix('js/estudante.js')) }}"></script> -->
-    <script src='{{ url(mix("js/$pagina.js")) }}'></script>
-    <!-- <script src="{{ url(mix('js/app.js')) }}"></script> -->
+<footer class="" id="contato">
+    <section class="container d-flex flex-sm-row justify-content-around text-light py-4" id="sobre">
+        <div class="">
+            <ul class="nav">
+                <li><a href="#" target="_white">@Unitech-lab</a></li>
+                <li>Endereço: Av Vitória, 950, Forte São João -
+                    Vitória/ES CEP: 29017-950</li>
+                <li>Telefone: 27 3331-8500</li>
+            </ul>
+        </div>
+    </section>
+</footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- <script src="{{ url(mix('js/estudante.js')) }}"></script> -->
+<script src='{{ url(mix("js/$pagina.js")) }}'></script>
+<!-- <script src="{{ url(mix('js/app.js')) }}"></script> -->
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @include('template.header')
 @include('template.nav')
-<main class="container text-center text-light mt-5" style="height: 70vh;">
+<main class="container text-center text-light mt-5">
     <!-- Avisos em Geral -->
     <div class="toast-container position-fixed bottom-0 right-0 p-3">
     </div>
