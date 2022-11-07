eba725cf770591bb59e4d22be1d221db5c653078
izaelgs <izael-123@outlook.com>
2022-11-07 19:33:53 -0300

    correcao link institucional nav

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-secondary" aria-current="page" href="./#sobre">Institucional</a>
+                    <a class="nav-link active text-secondary" aria-current="page" href="./">Institucional</a>
                 </li>
                 <li class="nav-item">
                     <div class="dropdown">
