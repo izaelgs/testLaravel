2b4ac43fe1927816d8d416cfb7c9372f572b1be8
izaelgs <izael-123@outlook.com>
2022-11-07 19:29:47 -0300

    titulo em producao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,12 @@
 @include('template.header')
 @include('template.nav')
-<main class="container text-center text-light mt-5">
+<main class="container text-center text-light mt-5 d-flex align-items-center justify-content-center">
     <!-- Avisos em Geral -->
     <div class="toast-container position-fixed bottom-0 right-0 p-3">
     </div>
     <!-- Apresentação -->
-    <section class="institucional mb-2 row d-flex align-items-center" id="sobre">
-        <!-- <div class="col-md-6">
-            <img src="img/GOHxPB_Balloon_BG.png" alt="" class="responsive">
-        </div>
-        <div class="col-md-6">
-            <h3>O Laboratório</h3>
-            <p>Suspendisse facilisis venenatis libero eu euismod. Etiam sed libero sit amet diam posuere tincidunt vitae sit amet nulla. Nullam pellentesque mollis purus. Proin quis leo ut felis suscipit scelerisque. Nulla ac metus nulla. Maecenas ac nisl ut enim fringilla sagittis. Phasellus quis nulla ex. Aenean in massa id magna consequat facilisis quis nec lacus. Maecenas molestie sollicitudin nisi, vel hendrerit tortor convallis a.</p>
-        </div> -->
+    <section class="institucional mb-2 row" id="sobre">
+        <h2 class="fw-bolder fs-1 text-secondary">Em Produção</h2>
     </section>
     <br>
     <!-- @include('formulario.formulario1')
